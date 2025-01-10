883d6b8837eecfbafed740f7e4950edd03f5721e
Robert On <robert@agency.fund>
2025-01-10 02:35:50 +0000

    Updates for packages, stripping references from docs.

--- plumber.R
+++ plumber.R
@@ -1,35 +1,35 @@
 library(plumber)
-library(tidyverse)
+library(stringr)
 library(httr2)
+library(tidyr)
+library(dplyr)
+library(purrr)
 
 claude_req <- function(content, endpoint = "messages", system_prompt=NULL,
                        query_prompt = "Derive a policy recommendation drawing from the documents in the provided content.",
-                       max_tokens = 1024) {
-  
+                       max_tokens = 1024, model = "claude-3-5-haiku-latest",
+                       api_version = "2023-06-01") {
   json_body <- list(
-    model = "claude-3-5-sonnet-20241022",
+    model = model,
     messages = list(
       list(role = "user",
            content = str_glue(
              "<content>{content}</content>{query_prompt}"))
     )
   )
-  
   if (!is.null(system_prompt)) {
     json_body$system = system_prompt
   }
-  
   if (endpoint == "messages") {
     json_body$max_tokens <- max_tokens
   } else if (endpoint == "messages/token_count") {
     
   }
-  
   json_resp <- request(str_glue("https://api.anthropic.com/v1/{endpoint}")) |>
     req_headers(
       `x-api-key` = readLines("anthropic_api_key.txt"),
       `content-type` = "application/text",
-      `anthropic-version` = "2023-06-01"
+      `anthropic-version` = api_version
     ) |>
     req_body_json(
       json_body
@@ -46,27 +46,31 @@ fetch_wb_docs <- function(query = "psychology interventions", num_results = 5) {
     resp_body_json() |>
     as_tibble() |>
     unnest_wider(documents) |>
-    # unnest_wider(where(~ is.list(.x)), names_sep = "_") |>
-    # unnest_wider(where(~ is.list(.x)), names_sep = "_") |>
-    # unite("authors", starts_with("authors_"), sep = ", ") |>
-    # mutate(authors = str_remove_all(authors, ", NA")) |>
-    # unite("keywords", starts_with("keywd_"), sep = ", ") |>
-    # mutate(keywords = str_remove_all(keywords, ", NA")) |>
     filter(!is.na(txturl)) |>
     mutate(txturl_resp = map(txturl, ~ request(.x) |> req_perform())) |>
-    # mutate(document = map_chr(txturl_resp, ~ .x |> resp_body_string())) |>
     mutate(txturl_body = map(txturl_resp, ~ .x |> resp_body_raw())) |>
     mutate(document = map_chr(txturl_body, ~ .x |> rawToChar() |> str_conv("UTF-8"))) |>
     mutate(txturl_code = map_int(txturl_resp, ~ .x |> resp_status())) |>
-    filter(txturl_code == 200) %>%
+    filter(txturl_code == 200) |>
     select(display_title, txturl, document)
-  
   return(wb_data)
 }
 
-doc_context <- function(docs, num_docs = NULL) {
+doc_context <- function(docs, num_docs = NULL, strip_references = TRUE,
+                        refs_thresh = 0.4) {
   if (!is.null(num_docs)) {
-    docs = docs[1:num_docs]
+    docs <- docs[1:num_docs]
+  }
+  for (i in 1:length(docs)) {
+    ref_idx <- str_locate_all(docs[i], "References")[[1]]
+    
+    if (nrow(ref_idx) > 0) {
+      last_ref_idx <- ref_idx[nrow(ref_idx), 1]
+      ref_pct <- last_ref_idx / str_length(docs[i])
+      if (ref_pct > refs_thresh) {
+        docs[i] <<- str_sub(docs[i], 1, last_ref_idx - 1)
+      }
+    }
   }
   str_c(
     "<document>",
@@ -100,7 +104,9 @@ function(query = "social psychology interventions", num_docs = 10,
   claude_res <- claude_req(context, query_prompt = query_prompt,
                            system_prompt = system_prompt)
   return(list(
-    response = claude_res$response$content[[1]]$text[[1]],
+    #response = claude_res$response$content[[1]]$text[[1]],
+    response = claude_res,
     sources = docs |> select(-document) |> head(num_docs)
   ))
 }
+
